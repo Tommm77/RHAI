1a7dd3c5da4cd78170546c2c68e0a5aa7069ea59
Tommm77 <picout.tom@gmail.com>
2024-06-18 14:48:52 +0200

    crud profile

--- back/back/urls.py
+++ back/back/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework.routers import DefaultRouter
+
+from api.views import ProfilViewSet
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -17,8 +20,12 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
+router = DefaultRouter()
+router.register(r'profils', ProfilViewSet)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
