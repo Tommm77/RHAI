48e864520708a2a151ee36c04742de5123ae2980
Tommm77 <picout.tom@gmail.com>
2024-06-19 14:55:55 +0200

    add bento grid to rh page

--- front/components/RH/RHHome.tsx
+++ front/components/RH/RHHome.tsx
@@ -1,11 +1,12 @@
 import {Navbar} from "@/components/HomePage/components/Navbar";
 import {CVList} from "@/components/RH/components/CVList";
-
+import {BentoDemo} from "@/components/RH/components/BentoGrid";
 export const RHHome = () => {
     return (
         <div className="h-full w-full">
             <Navbar />
             <CVList />
+            <BentoDemo />
         </div>
     );
 }
