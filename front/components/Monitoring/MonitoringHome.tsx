ffbf5c8f120ec4f951924581b273b7962d3bfe74
Playinga18 <91030442+Playinga18@users.noreply.github.com>
2024-06-21 00:18:05 +0200

    route to swagger and admin

--- front/components/Monitoring/MonitoringHome.tsx
+++ front/components/Monitoring/MonitoringHome.tsx
@@ -1,7 +1,6 @@
 import {Navbar} from "@/components/HomePage/components/Navbar";
-import AnimatedGradientText from "../magicui/animated-gradient-text";
 import {Button} from "@/components/ui/button";
-import {Profiltabs} from "@/components/Profil/components/ProfilTabs";
+import Link from "next/link";
 
 export const MonitoringHome = () => {
     return (
@@ -9,8 +8,16 @@ export const MonitoringHome = () => {
             <Navbar />
             <div className="flex flex-col justify-center items-center mt-20">
                 <div className="flex space-x-10">
-                    <Button className="rounded-xl">accéder admin</Button>
-                    <Button className="rounded-xl">accéder swagger</Button>
+                    <Button className="rounded-xl">
+                        <Link href="https://rhai-api.vercel.app/admin/" className="rounded-2xl">
+                            <p>accéder admin</p>
+                        </Link>
+                    </Button>
+                    <Button className="rounded-xl">
+                        <Link href="https://rhai-api.vercel.app/swagger/" className="rounded-2xl">
+                            <p>accéder swagger</p>
+                        </Link>
+                    </Button>
                 </div>
             </div>
         </div>
